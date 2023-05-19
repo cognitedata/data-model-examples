407fde740bc39399be21a5bf871d03397ada9ee2
Greger Wedel <greger.wedel@cognite.com>
2023-05-19 08:13:42 +0200

    Cleanup

--- {{cookiecutter.buildfolder}}/load_data.py
+++ {{cookiecutter.buildfolder}}/load_data.py
@@ -1,36 +1,23 @@
-import csv
+#!/usr/bin/env python
 import os
 import click
 import sys
 from cognite.client import CogniteClient, ClientConfig
 from cognite.client.credentials import OAuthClientCredentials
-from cognite.client.data_classes.raw import Row
 from dotenv import load_dotenv
 from inventory import examples_config
 import pandas as pd
-
-gettrace = sys.gettrace()
-debug_status = True if gettrace else False
-
-if debug_status:
-    load_dotenv("../.env")
-    examples_config = {
-        "movie_actors": {
-            "raw_db": "test_movies",
-        },
-        "apm_simple": {
-            "raw_db": "test_apm_simple",
-        },
-    }
-else:
-    load_dotenv()
 
 
 @click.command()
 @click.option("--file", default="", help="Specify a specific table to load.")
 @click.option("--drop", default=False, help="Whether to delete existing data.")
 @click.argument("example_folder")
-def load(file, drop, example_folder):
+def cli(file: str, drop: bool, example_folder: str):
+    load_raw(file, drop, example_folder)
+
+
+def load_raw(file, drop, example_folder):
     # This creation of a CogniteClient() uses client credentials and environment variables.
     # This approach can be reused for any usage of the Cognite Python SDK.
     client = CogniteClient(
@@ -53,16 +40,18 @@ def load(file, drop, example_folder):
         # correct access for what you want to do.
         resp = client.get("/api/v1/token/inspect").json()
         if resp is None or len(resp) == 0:
-            print(f"CDF client does not have access to your project.")
+            click.echo(f"CDF client does not have access to your project.")
             exit(1)
     except:
-        print("Failure when trying to check login status with CDF.")
+        click.echo("Failure when trying to check login status with CDF.")
         exit(1)
     # The name of the raw database to create is picked up from the inventory.py file, which
     # again is templated with cookiecutter based on the user's input.
     raw_db = examples_config.get(example_folder, {}).get("raw_db", "")
     if raw_db == "":
-        print(f"Could not find raw_db in inventory.py for example {example_folder}.")
+        click.echo(
+            f"Could not find raw_db in inventory.py for example {example_folder}."
+        )
         exit(1)
     try:
         if drop:
@@ -72,7 +61,7 @@ def load(file, drop, example_folder):
                     client.raw.tables.delete(raw_db, table.name)
             client.raw.databases.delete(raw_db)
     except:
-        print(
+        click.echo(
             f"Failed to delete {raw_db} for example {example_folder}. It may not exist."
         )
     try:
@@ -82,7 +71,9 @@ def load(file, drop, example_folder):
         # However, it is included to show how you can use the SDK.
         client.raw.databases.create(raw_db)
     except Exception as e:
-        print(f"Failed to create {raw_db} for example {example_folder}: {e.message}")
+        click.echo(
+            f"Failed to create {raw_db} for example {example_folder}: {e.message}"
+        )
         exit(1)
     files = []
     if file:
@@ -90,18 +81,13 @@ def load(file, drop, example_folder):
         files.append(file)
     else:
         # Pick up all the .csv files in the data folder of the example.
-        for _, _, filenames in os.walk(f"./{example_folder}/data/"):
+        for _, _, filenames in os.walk(f"./{example_folder}/data/raw"):
             for f in filenames:
                 if ".csv" in f:
                     files.append(f)
-    key = examples_config.get(example_folder, {}).get("raw_db_key", "")
-    if key == "":
-        raise BaseException(
-            "Could not find raw_db_key in inventory.py for example {example_folder}."
-        )
     for f in files:
         with open(f"./{example_folder}/data/{f}", "rt") as file:
-            print(f"Uploading {f} to {raw_db}...")
+            click.echo(f"Uploading {f} to {raw_db}...")
             dataframe = pd.read_csv(file, dtype=str)
             dataframe = dataframe.fillna("")
             try:
@@ -112,11 +98,28 @@ def load(file, drop, example_folder):
                     ensure_parent=True,
                 )
             except Exception as e:
-                print(f"Failed to upload {f} for example {example_folder}: {e.message}")
+                click.echo(
+                    f"Failed to upload {f} for example {example_folder}: {e.message}"
+                )
                 exit(1)
-        print(f"Successfully uploaded {f} to {raw_db}.")
-    print(f"Successfully uploaded {len(files)} files to {raw_db}.")
+        click.echo(f"Successfully uploaded {f} to {raw_db}.")
+    click.echo(f"Successfully uploaded {len(files)} files to {raw_db}.")
 
 
 if __name__ == "__main__":
-    load()
+    gettrace = sys.gettrace()
+    debug_status = True if gettrace else False
+
+    if debug_status:
+        load_dotenv("../.env")
+        examples_config = {
+            "movie_actors": {
+                "raw_db": "test_movies",
+            },
+            "apm_simple": {
+                "raw_db": "test_apm_simple",
+            },
+        }
+    else:
+        load_dotenv()
+    cli()
