28037a32a7d3f3677d75cfc63d1341281ee4c2f1
Greger Wedel <greger.wedel@cognite.com>
2023-05-23 14:08:16 +0200

    Minor fixes found in call w/Anders

--- {{cookiecutter.buildfolder}}/cdf-login.sh
+++ {{cookiecutter.buildfolder}}/cdf-login.sh
@@ -4,12 +4,12 @@
 # Use set -a; source .env; set +a
 # from your command line to load the variables into your current shell.
 set -a 
-source ../.env
+source ./.env
 set +a
 echo "Logging into CDF using environment variables from .env..."
 cdf login --client-id=$CDF_CLIENT_ID --client-secret=$CDF_CLIENT_SECRET --tenant=$CDF_TENANT_ID --cluster=$CDF_CLUSTER $CDF_PROJECT
 cdf status
 
-echo "Testing by listing transformstions..."
+echo "Testing by listing transformations..."
 # This will also work when .env variables are loaded: transformations-cli list
 transformations-cli --token-url=$CDF_TOKEN_URL --client-secret=$CDF_CLIENT_SECRET --client-id=$CDF_CLIENT_ID --scopes=$CDF_SCOPES --cluster=$CDF_CLUSTER --cdf-project-name=$CDF_PROJECT list
